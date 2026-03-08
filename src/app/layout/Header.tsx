3290e3121f735fb6a5c0e76cad3cd97902d94cd3
antony-k-sebastian <antony_k_sebastian@outlook.com>
2026-03-08 12:44:32 +0000

    feat: add searchbar and logo to header

--- src/app/layout/Header.tsx
+++ src/app/layout/Header.tsx
@@ -3,22 +3,30 @@ import styles from "./Header.module.css";
 import React from "react";
 import MessageButton from "../../shared/components/Button/MessageButton";
 import ProfileButton from "../../shared/components/Button/ProfileButton";
+import SearchBar from "../../shared/components/Button/SearchBar";
 
 function Header() {
   return (
     <header className={styles.header}>
-      <nav className={styles.nav}>
+      <div className={styles.left}>
+        <span className={styles.logo}>
+          <h1>Sellby</h1>
+          <h1>MU</h1>
+        </span>
+        <SearchBar placeholder="Calculator fx 99..." />
+      </div>
+      <div className={styles.right}>
         <MessageButton
           label="Messages"
           count={5}
           onClick={() => console.log("Messages clicked")}
         />
-        <ProfileButton 
+        <ProfileButton
           name="Akiljith Kumar"
           avatarUrl="pass the actual avatar url here"
           onClick={() => console.log("Profile clicked")}
         />
-      </nav>
+      </div>
     </header>
   );
 }
