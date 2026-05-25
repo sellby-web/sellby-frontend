b5700c2775d68a05345b26a2331b6e1679626ff1
antony-k-sebastian <antony_k_sebastian@outlook.com>
2026-05-25 18:13:08 +0100

    feature/Implement upload image button and placeholder for image

---
+++ src/features/ads/components/UploadImage/UploadImage.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import styles from "./UploadImage.module.css";
+
+function UploadImage() {
+  return (
+    <div className={styles.uploadContainer}>
+      <button className={styles.uploadButton}>Upload Image
+        <img
+          src="/icons/UploadIcon.svg"
+          alt="Upload"
+          className={styles.uploadIcon}
+        />
+      </button>
+      <div className={styles.imageGrid}>
+        <div className={styles.imageBox}></div>
+        <div className={styles.imageBox}></div>
+        <div className={styles.imageBox}></div>
+        <div className={styles.imageBox}></div>
+      </div>
+    </div>
+  );
+}
+
+export default UploadImage;
