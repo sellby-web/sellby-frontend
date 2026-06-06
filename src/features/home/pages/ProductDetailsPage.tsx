2dafdf6f7d9a29413caf17f59ac6e01504683e31
antony-k-sebastian <antony_k_sebastian@outlook.com>
2026-06-06 22:44:08 +0100

    feat: add product details page and remove unused files

---
+++ src/features/home/pages/ProductDetailsPage.tsx
@@ -0,0 +1,88 @@
+import React from "react";
+import Header from "../../../app/layout/Header";
+import Sidebar from "../../../shared/components/Modal/Sidebar";
+import styles from "./ProductDetailsPage.module.css";
+
+type ProductDetailsPageProps = {
+  imgUrl: string;
+  productTitle: string;
+  name: string;
+  price: number;
+  timeStamp: string;
+};
+
+function ProductDetailsPage({
+  imgUrl,
+  productTitle,
+  name,
+  price,
+  timeStamp,
+}: ProductDetailsPageProps) {
+  return (
+    <div className={styles.detailsPageContainer}>
+      <Header />
+      <div className={styles.detailsPageContent}>
+        <Sidebar />
+        <main className={styles.detailsPageMain}>
+          {/** Back Button */}
+          <div>
+            <button className={styles.backButton}>
+              <img
+                src={"icons/BackIcon.svg"}
+                alt="Back Icon"
+                className={styles.backIcon}
+              />
+            </button>
+          </div>
+
+          <h2 className={styles.productTitle}>
+            Introduction to Theory of Computation - Micheal Sipser. New and not
+            damaged. Clean pages. Latest Edition.
+          </h2>
+
+          {/** Seller Information */}
+          <div className={styles.sellerInfo}>
+            <div className={styles.userImageContainer}>
+              {imgUrl && (
+                <img
+                  src={imgUrl}
+                  alt="User Image"
+                  className={styles.userImage}
+                />
+              )}
+            </div>
+            <h3 className={styles.sellerName}>Akhiljith Kumar</h3>
+            <button className={styles.messageButton}>Message</button>
+          </div>
+
+          {/** Product Images */}
+          <div className={styles.imageGrid}>
+            <div className={styles.imageBox}></div>
+            <div className={styles.imageBox}></div>
+            <div className={styles.imageBox}></div>
+            <div className={styles.imageBox}></div>
+          </div>
+
+          <p className={styles.productDescription}>
+            This is a brief description of the product. To ensure that the
+            description style is applied to the content as expected. This will
+            help with assuring that the css developement held for this project
+            is going on its absolute peak. But gladly thats not the case.
+          </p>
+
+          <p className={styles.productDescription}>
+            Subject: Computer Science <br />
+            Author: Micheal Sipser <br />
+          </p>
+
+          <div className={styles.footer}>
+            <h2 className={styles.price}>€5.00</h2>
+            <p className={styles.timeStamp}> 2 hours ago</p>
+          </div>
+        </main>
+      </div>
+    </div>
+  );
+}
+
+export default ProductDetailsPage;
